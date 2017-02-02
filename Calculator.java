6b56481d4494a3f090a193ed3811e2f47dc578e6
hnshin <gkgkgk0001@gmail.com>
2017-02-02 16:15:58 +0900

    Calculator add

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+
+
+public class Calculator{
+
+	public int add(int x, int y){
+	
+		return x + y;
+	}
+
+	public int substract(int x, int y){
+
+		return x - y;
+	}
+
+}
--- Hello.java
+++ Hello.java
@@ -5,5 +5,10 @@ public class Hello{
 		for(int i=0; i<10; i++)
 		System.out.println("Hello Git!!");
 
+		Calculator c = new Calculator();
+
+		System.out.println("3 + 4 = " + c.add(3,4));
+		System.out.println("3 - 4 = " + c.substract(3,4));	
+
 	}
 }
